c258dd6e474f2b6bdc823692bd587afcf0798d58
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-22 23:47:03 +0200

    Fix middleware for async actions issues

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import pizzaReducer from './pizza';
 
 const mainReducer = combineReducers({
-  pizzaReducer,
+  pizza: pizzaReducer,
 });
 
-const store = createStore(mainReducer);
+const store = createStore(mainReducer, applyMiddleware(thunk));
 
 export default store;
