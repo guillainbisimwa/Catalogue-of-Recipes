278e7962f57e4c349ff0f9a4fc1ddb2f22ebbfac
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-23 23:07:48 +0200

    Add Steak reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import burgerReducer from './burger';
 import pizzaReducer from './pizza';
+import steakReducer from './steak';
 
 const mainReducer = combineReducers({
   pizza: pizzaReducer,
   burger: burgerReducer,
+  steak: steakReducer,
 });
 
 const store = createStore(mainReducer, applyMiddleware(thunk));
