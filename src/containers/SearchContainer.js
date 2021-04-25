5b10d02d09b9f830bf6a978320fd44a07dae012a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-25 13:01:20 +0200

    Add filter component

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,24 +2,27 @@ import {
   Box, Grid, MenuItem, TextField,
 } from '@material-ui/core';
 
-const SearchContainer = () => (
+const SearchContainer = () => {
+  const categories = ['All', 'Pizza', 'Burger', 'Steak'];
+  const categoryList = categories.map((category) => (
+    <MenuItem key={category} value={category}>
+      {category}
+    </MenuItem>
+  ));
+  return (
     <Box p={5}>
       <Grid item xs={12}>
         <TextField
           select
           fullWidth
-          label="Select"
+          label="Select the category from the drop-down menu"
           variant="outlined"
         >
-          <MenuItem >
-            Test 1
-          </MenuItem>
-          <MenuItem >
-            Test 2
-          </MenuItem>
+          {categoryList}
         </TextField>
       </Grid>
     </Box>
-);
+  );
+};
 
 export default SearchContainer;
