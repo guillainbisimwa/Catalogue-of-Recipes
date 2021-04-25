bfb8019d31d93933704e7b056a408f018472209e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-25 13:32:52 +0200

    Add filter reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import filterReducer from './filter';
 import burgerReducer from './burger';
 import pizzaReducer from './pizza';
 import steakReducer from './steak';
@@ -8,6 +9,7 @@ const mainReducer = combineReducers({
   pizza: pizzaReducer,
   burger: burgerReducer,
   steak: steakReducer,
+  filter: filterReducer,
 });
 
 const store = createStore(mainReducer, applyMiddleware(thunk));
