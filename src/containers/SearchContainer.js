884786bb6ca4871eddfb38c3e9ca0260182000ce
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-26 17:25:08 +0200

    Fix the select default category bug

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,8 +2,16 @@ import {
   Box, Grid, MenuItem, TextField,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const SearchContainer = ({ filterHandler }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+    filterHandler(event.target.value);
+  };
+
   const categories = ['All', 'Pizza', 'Burger', 'Steak'];
   const categoryList = categories.map((category) => (
     <MenuItem key={category} value={category}>
@@ -14,8 +22,9 @@ const SearchContainer = ({ filterHandler }) => {
     <Box p={5}>
       <Grid item xs={12}>
         <TextField
-          onChange={(e) => filterHandler(e.target.value)}
+          onChange={onChange}
           select
+          value={value}
           fullWidth
           label="Select the category from the drop-down menu"
           variant="outlined"
